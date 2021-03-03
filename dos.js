daea7c521db488b8fe31d41a68bd48b81bdfde83
Jorge <joregat@gmail.com>
2021-03-03 10:53:35 -0300

    Parcial terminado

--- dos.js
+++ dos.js
@@ -1,4 +1,134 @@
-function mostrar()
-{
-  alert("dos");
+function mostrar() {
+  let nombre;
+  let cursada;
+  let sexo;
+  let nota;
+  let edad;
+  let cantidad; 
+  let notaMax;
+  let nombreNotaMax;
+  let nombreMasJovenL;
+  let edadMasJovenL;
+  let promedioF;
+  let promedioM;
+  let promedioNobinario;
+  let flagMasCursos = 1;
+  let edadMasCursos;
+  let nombreMasCursos;
+  let materiasMax; 
+  let seguir;
+  let acumM = 0;
+  let contNotasM = 0
+  let acumF = 0; 
+  let contNotasF = 0;
+  let acumNoB = 0;
+  let contNotasNoB = 0;
+  let flagNotaMax = 1;
+  let flagEdadMin = 1;
+
+  do {
+    nombre = prompt("Ingrese el nombre: ");
+    cursada = prompt("Ingrese tipo de cursada: ");
+    while (
+      cursada != "Libre" &&
+      cursada != "Presencial" &&
+      cursada != "Remota"
+    ) {
+      cursada = prompt(
+        "Dato inválido. Reingrese el tipo de cursada (Libre, Presencial o Remota): "
+      );
+    }
+    cantidad = parseInt(prompt("Ingrese la cantidad de materias: "))
+    while (isNaN(cantidad) || cantidad <= 0 || cantidad > 8){
+      cantidad = parseInt(prompt("Error, reingrese la cantidad de materias: "))
+    }
+    sexo = prompt("Ingrese estado sexo: f/m/no binario");
+    while (sexo != "f" && sexo != "m" && sexo != "no binario") {
+      sexo = prompt("Error, reingrese estado sexo: f/m/ no binario");
+    }
+    nota = parseInt(prompt("Ingrese nota promedio: "));
+    while (isNaN(nota) || nota < 0 || nota > 10) {
+      nota = parseInt(prompt("Error, reingrese nota promedio: "));
+    }
+    edad = parseInt(prompt("Ingrese edad: "));
+    while (isNaN(edad) || edad < 0) {
+      edad = parseInt(prompt("Error, reingrese edad: "));
+    }
+    if (sexo == "f" || sexo == "no binario"){
+      if (flagNotaMax == 1 || nota > notaMax){
+        notaMax = nota;
+        nombreNotaMax = nombre; 
+        flagNotaMax = 0; 
+      }
+    }
+    if (cursada == "Libre"){
+      if (flagEdadMin || edad < edadMasJovenL){
+        edadMasJovenL = edad;
+        nombreMasJovenL = nombre; 
+        flagEdadMin = 0;
+    }
+  }
+  if(cursada == "Libre" || cursada == "Presencial"){
+    if (flagMasCursos || cantidad > materiasMax){
+      materiasMax = cantidad;
+      edadMasCursos = edad;
+      nombreMasCursos = nombre;
+      flagMasCursos = 0;
+    }
+  }
+  switch (sexo){
+    case "f":
+      contNotasF++;
+      acumF += nota;
+      break;
+    case "m":
+      contNotasM++;
+      acumM+= nota;
+      break;
+    case "no binario":
+      contNotasM++;
+      acumNoB += nota;
+  }
+    seguir = prompt("¿Quiere ingresar otro alumno: (si/no)"); 
+} while (seguir == "si");
+
+if ( notaMax > 0){
+console.log ("A- El promedio más alto de un estudiante no masculino es: " + nombreNotaMax + " con un promedio de " + notaMax);
+} else {
+console.log ("A- No hay alumnas femeninas o no binarios");
 }
+if (edadMasJovenL > 0){
+console.log ("B- El nombre del mas joven de los alumnos entre los que la dan libre es: " + nombreMasJovenL + " con " + edadMasJovenL + " años");
+}
+else {
+  console.log("B- No hay alumnos que cursen libre");
+}
+if (contNotasF > 0 ){
+  promedioF = acumF/contNotasF;
+  console.log ("C- El promedio de notas femeninos es: " + promedioF);
+}
+else {
+  console.log ("C- No hay alumnas femeninas");
+}
+if (contNotasM > 0){
+  promedioM = acumM/contNotasM;
+  console.log ("C- El promedio de notas masculinos es: " + promedioM);
+}
+else {
+  console.log ("C- No hay alumnos masculinos");
+}
+if (contNotasNoB > 0){
+  promedioNobinario = acumNoB/contNotasNoB;
+  console.log ("C- El promedio de notas masculinos es: " + promedioNobinario);
+} else {
+  console.log ("C- No hay alumnos no binarios");
+}
+if (cursada != "Remota"){
+  console.log("D- La edad y nombre del que cursa mas materias que no sean en forma remota es de: " + nombreMasCursos + " con " + edadMasCursos + " y con " + materiasMax + " materias");
+}
+else {
+  console.log("D- No hay alumnos que cursen forma libre o presencial"); 
+}
+}
+
+// Jorge García Div B, DNI: 95873374
--- uno.js
+++ uno.js
@@ -1,5 +1,104 @@
-
 function mostrar()
 {
-	alert("uno");
-}
+	let nombre;
+	let tipo;
+	let cantidad;
+	let precio;
+	let tipoDeInflamable;
+	let marca;
+	let promedioAlcohol;
+	let promedioIac;
+	let promedioQuat; 
+	let acumAlcohol = 0;
+	let acumIac = 0; 
+	let acumQuat = 0; 
+	let contAlcohol = 0;
+	let contIac = 0;
+	let contIacBarato = 0;
+	let acumIacBarato = 0; 
+	let contQuat = 0; 
+	let precioMax;
+	let marcaMax; 
+	let tipoMax; 
+	let acumIgnifugo = 0;
+	let acumCombustible = 0;
+	let acumExplosivo = 0;
+	let flag = 1;
+
+	for (let i = 0; i < 5; i++) {
+		nombre = prompt("Ingrese el nombre del producto: ");
+		tipo = prompt("Ingrese el tipo de producto (Alcohol, Iac o Quat): ");
+		while (tipo != "Alcohol" && tipo != "Iac" && tipo != "Quat"){
+			tipo = prompt("Dato inválido. Reingrese el producto (Alcohol, Iac o Quat): ");
+		}
+		precio = parseInt(prompt("Ingrese precio en 100 y 300 pesos: "));
+		while (isNaN(precio) || precio < 100 || precio > 300) {
+			precio = parseInt(prompt("Error, reingrese precio entre 100 y 300 pesos: "));
+		}
+		cantidad = parseInt(prompt("Ingrese cantidad de productos (hasta 1000 unidades): "))
+		while (cantidad <= 0 || cantidad > 1000 || isNaN(cantidad)) {
+			cantidad = parseInt(prompt("Error, reingrese cantidad de unidades (hasta 1000 unidades): "));
+		} 
+		tipoDeInflamable = prompt("Ingrese el tipo de inflamabele (ignífugo/ combustible/ explosivo): ")
+		while(tipoDeInflamable != "ignífugo" && tipoDeInflamable != "combustible" && tipoDeInflamable != "explosivo"){
+			tipoDeInflamable = prompt("Error, reingrese el tipo de inflamabele (ignífugo/ combustible/ explosivo): ");
+		}
+		marca = prompt("Ingrese marca: ")
+
+		switch (tipo){
+			case "Alcohol":
+				acumAlcohol += cantidad;
+				contAlcohol++;
+				break;
+			case "Iac":
+				acumIac += cantidad;
+				contIac++;
+				if (precio <= 200){
+					contIacBarato++;
+					acumIacBarato += cantidad;
+				}
+				break;
+			case "Quat":
+				acumQuat+= cantidad;
+				contQuat++;
+				break;
+		}
+		if(tipoDeInflamable == "ignífugo"){
+			acumIgnifugo += cantidad;
+		} 
+		else if ( tipoDeInflamable == "combustible"){
+			acumCombustible+= cantidad;
+		}
+		else if (tipoDeInflamable == "explosivo"){
+			acumExplosivo += cantidad;
+		}
+
+		if (flag || precio > precioMax){
+			precioMax = precio;
+			marcaMax = marca;
+			tipoMax = tipo;
+			flag = 0;
+		}
+	}
+	promedioIac = acumIac/contIac;
+	promedioQuat = acumQuat/contQuat;
+	promedioAlcohol = acumAlcohol/contAlcohol;
+	
+	console.log("A- El promedio de cantidad de alcoles es de: " + promedioAlcohol);
+	console.log("A- El promedio de cantidad de QUAT es de: " + promedioQuat);
+	console.log("A- El promedio de cantidad de IAC es de: " + promedioIac);
+
+	if (acumIgnifugo > acumCombustible && acumIgnifugo > acumExplosivo){
+		console.log("B- El tipo de inflamable con más cantidad de unidades en total es ignifugo con : " + acumIgnifugo);
+	}
+	else if (acumCombustible >= acumIgnifugo && acumCombustible > acumExplosivo){
+		console.log("B- El tipo de inflamable con más cantidad de unidades en total es combustible con : " + acumCombustible);
+	}
+	else {
+		console.log("B- El tipo de inflamable con más cantidad de unidades en total es el explosivo con : " + acumExplosivo);
+	}
+
+	console.log("C- La cantidad de IAC con precios menos a 200 es de: " + contIacBarato);
+
+	console.log("D- El tipo más caro es: " + tipoMax + " de marca " + marcaMax + " con un precio de $" + precioMax);
+} // Jorge García Div B, DNI: 95873374
